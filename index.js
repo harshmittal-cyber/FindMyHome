af64b1d1986489461486d452eafa1727ac0c1958
Harsh Mittal <mittalharsh4321@gmail.com>
2021-03-23 12:52:03 +0530

    Deploying on heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 const cookieParser = require("cookie-parser");
 const expressLayouts = require("express-ejs-layouts");
 const db = require("./config/mongoose");
