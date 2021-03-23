aea9f4275373f2522cc367745f5fb06209e6dbcb
Harsh Mittal <mittalharsh4321@gmail.com>
2021-03-23 13:48:54 +0530

    connect database

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,8 +4,10 @@ mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 mongoose.set("useUnifiedTopology", true);
 
-mongoose.connect("mongodb://localhost/findmyhomes");
-
+// mongoose.connect("mongodb://localhost/findmyhomes");
+mongoose.connect(
+  "mongodb+srv://harsh_mittal18:M9o6hPRPVBEWhixr@findmyhomes.irc4s.mongodb.net/findmyhomes?retryWrites=true&w=majority"
+);
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
